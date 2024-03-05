74982c8496fb1628453920a5bfb13a513602e10e
Yakuma Kuramy <104542682+YakumaKuramy@users.noreply.github.com>
2024-03-05 10:26:44 -0300

    Atualização
    Primeiros codigos, com base nos estudos do curso Java na DIO

---
+++ Atividade DIO/java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,5 @@
+public class MinhaClasse { // <- o nome da classe principal precisa ser o mesmo que o do projet/arquivo
+    public static void main(String[] args) { // <- metodo | "main" primeiro metodo a ser chamado
+        System.out.println("Olá, seja bem vindo ao meu codigo!"); // <- saida/impressão em tela da mensagem
+    }
+}
